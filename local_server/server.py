be1b29c1a16a512595cdf7922c9489f13ef5a62e
wisemonkey <oranbusiness@gmail.com>
2020-01-18 09:21:24 -0800

    implemented server url

--- local_server/server.py
+++ local_server/server.py
@@ -23,7 +23,7 @@ def record_loop(loop_on):
             arduino_button_state = ser.readline().decode("utf-8").strip() #decode byte string from arduino and strip return characters
             print(repr(arduino_button_state)) # Read the newest output from the Arduino
             
-            re = requests.get("https://en459uflysvc8.x.pipedream.net/"+arduino_button_state)
+            re = requests.get(SERVER_URL+arduino_button_state)
             print(re)  
             
 def exit_cleanup():
