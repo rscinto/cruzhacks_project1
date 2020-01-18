f21612d354b82f263abb27d45625a1b220192219
wisemonkey <oranbusiness@gmail.com>
2020-01-18 10:33:41 -0800

    added open trash can on verbal command to audio_local

--- local_server/audio_local.py
+++ local_server/audio_local.py
@@ -1,15 +1,27 @@
 import speech_recognition as sr
 import pyaudio
 
+recycling = ["can","cup"]
+trash = ["chip","bag"]
+
 # get audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Speak:")
     audio = r.listen(source, phrase_time_limit=5.0)
-    print(type(audio))
-# "16d5e7247d859dbb052b431e562a3fa09a56f6e9"
 try:
-    print("You said " + r.recognize_google(audio))
+    raw_text = r.recognize_google(audio)
+    print("Result " + raw_text)
+    for recycling_name in recycling:
+        print(recycling_name)
+        if recycling_name in raw_text:
+            print("Open Recycle can!")
+            break
+    for trash_name in trash:
+        if trash_name in raw_text:
+            print("Open Trash can!")
+            break
+    
 except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
