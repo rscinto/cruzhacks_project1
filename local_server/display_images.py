29fd3855a4049c6284e906017fcd09225095de36
wisemonkey <oranbusiness@gmail.com>
2020-01-19 10:48:11 -0800

    revierted webbrowser.get to use internet explorere

--- local_server/display_images.py
+++ local_server/display_images.py
@@ -3,16 +3,16 @@ import pathlib
 import os,webbrowser
 
 
-print(webbrowser._browsers)
+
 def show_photo(name):
     name_ = name + ".jpg"
     img_path = pathlib.Path.cwd() / "local_server" / "img" / name_
     if img_path.is_file():
         iexplore = os.path.join(os.environ.get("PROGRAMFILES", "C:\\Program Files"),
                                 "Internet Explorer\\IEXPLORE.EXE")
-        browser = webbrowser.get(	"chromium")
+        browser = webbrowser.get(iexplore)
         browser.open(str(img_path))
     else:
         print("Item not in database found")
-# show_photo("bag")
+show_photo("bag")
 show_photo("card")
